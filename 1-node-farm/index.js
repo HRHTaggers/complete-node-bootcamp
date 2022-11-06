67d421672a82ce614cdb49ecf7d18cb96fd1ca67
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-11-06 21:35:55 +0000

    Callback Hell exercise

---
+++ 1-node-farm/index.js
@@ -0,0 +1,27 @@
+const fs = require(`fs`);
+
+//BLOCKING, SYNCHRONOUS CODE
+/*
+const textIn = fs.readFileSync(`./starter/txt/input.txt`, `utf-8`);
+console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}.`;
+fs.writeFileSync(`./starter/txt/output.txt`, textOut);
+console.log(`File written`);
+*/
+
+//NON-BLOCKING, ASYNCHRONOUS CODE
+fs.readFile(`./starter/txt/start.txt`, `utf-8`, (err, data1) => {
+    if (err) return console.log(`Error! ❌`);
+
+    fs.readFile(`./starter/txt/${data1}.txt`, `utf-8`, (err, data2) => {
+        console.log(data2);
+        fs.readFile(`./starter/txt/append.txt`, `utf-8`, (err, data3) => {
+          console.log(data3);
+
+          fs.writeFile(`./starter/txt/final.txt`, `${data2}\n${data3}`, `utf-8`, err => {
+            console.log(`Your file has been written 🌟`);
+          })
+        });
+    });
+});
